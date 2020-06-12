eed64b56849408ae7952ac1583aeda6930889ccb
mbake94 <mbake94@gmail.com>
2020-06-12 11:32:12 -0700

    staging

--- src/main/java/com/example/demo/SpringSecurity405Application.java
+++ src/main/java/com/example/demo/SpringSecurity405Application.java
@@ -1,13 +1,8 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSecurity405Application {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SpringSecurity405Application.class, args);
-    }
 
 }
